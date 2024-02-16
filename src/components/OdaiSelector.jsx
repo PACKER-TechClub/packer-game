34477fd71caf4a14ff569085f1ccdd9ecf94bf78
bearone236 <kuma_2306@icloud.com>
2024-02-16 15:22:33 +0900

    簡易的スタイリング

--- src/components/OdaiSelector.jsx
+++ src/components/OdaiSelector.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 
-export default function({ OdaiList }) {
-    const [odai, setOdai] = useState(null);
+export default function ({ OdaiList }) {
+  const [odai, setOdai] = useState(null);
 
-    const selectOdai = () => {
-        const index = Math.floor(Math.random() * OdaiList.length);
-        setOdai(OdaiList[index]);
-    }
+  const selectOdai = () => {
+    const index = Math.floor(Math.random() * OdaiList.length);
+    setOdai(OdaiList[index]);
+  };
 
-    return (
-        <>
-            <button onClick={selectOdai}>お題を選ぶ</button>
-            <div>
-                {odai &&
-                    <>
-                        <h2>お題は</h2><h1>{odai}</h1>
-                    </>}
-            </div>
-        </>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <button
+        onClick={selectOdai}
+        className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out my-4"
+      >
+        お題を選ぶ
+      </button>
+      <div className="mt-5 w-full text-center">
+        {odai && (
+          <>
+            <h2 className="text-lg text-gray-700">お題は</h2>
+            <h1 className="text-2xl md:text-4xl font-bold text-orange-500">
+              {odai}
+            </h1>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
